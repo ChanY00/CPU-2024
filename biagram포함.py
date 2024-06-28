73b852d9ae07e7587b0c85a47b1566227377e376
ChanY00 <csdud77@naver.com>
2024-06-28 10:46:02 +0900

    수정

--- biagram포함.py
+++ biagram포함.py
@@ -8,20 +8,20 @@ import spacy
 from nltk import bigrams
 
 # 엑셀 파일 불러오기
-file_path = 'C:\\Users\\dnjsr\\Desktop\\특허 데이터 분석 참고 자료\\코드\\데이터\\디지털웰에이징_초거대인공지능.xlsx'  # 파일 경로 지정
+file_path = "C:\\Users\\dnjsr\\Desktop\\캠퍼스 유니버시아드\\2024년_캠퍼스_특허유니버시아드_특허데이터_한국특허전략개발원.xlsx"  # 파일 경로 지정
 df = pd.read_excel(file_path)
 
 # 필요한 열에 대한 전처리 수행
-df['요약(원문)'] = df['요약(원문)'].astype(str)
-df['요약(원문)'] = df['요약(원문)'].apply(lambda x: x.upper())  # 대문자 변환 혹은 다른 필요한 처리
+df['요약'] = df['요약'].astype(str)
+df['요약'] = df['요약'].apply(lambda x: x.upper())  # 대문자 변환 혹은 다른 필요한 처리
 
 # 함수 정의: 특수 문자 제거
 def remove_special_characters(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     return text
 
-# '요약(원문)' 열에 적용하여 특수 문자 제거
-df['요약(원문)'] = df['요약(원문)'].astype(str).apply(remove_special_characters)
+# '요약' 열에 적용하여 특수 문자 제거
+df['요약'] = df['요약'].astype(str).apply(remove_special_characters)
 
 
 # 불용어 제거 코드
@@ -42,7 +42,7 @@ def remove_stopwords(text):
     return filtered_text
 
 # 불용어 제거 및 대체 작업 적용
-df['요약(원문)'] = df['요약(원문)'].astype(str).apply(remove_stopwords)
+df['요약'] = df['요약'].astype(str).apply(remove_stopwords)
 
 #표제어추출
 # WordNet 데이터 다운로드 (표제어 추출을 위해 필요)
@@ -51,9 +51,9 @@ nltk.download('wordnet')
 # WordNetLemmatizer 객체 생성
 lemmatizer = WordNetLemmatizer()
 
-# 여러분의 DataFrame('df')을 가정하고 있는데, 여기서 '요약(원문)' 컬럼을 가정합니다.
+# 여러분의 DataFrame('df')을 가정하고 있는데, 여기서 '요약' 컬럼을 가정합니다.
 # 해당 컬럼의 각 단어를 lemmatize하여 다시 컬럼에 할당하는 예시입니다.
-df['요약(원문)'] = df['요약(원문)'].astype(str).apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
+df['요약'] = df['요약'].astype(str).apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
 
 # SpaCy 모델 로드
 nlp = spacy.load("en_core_web_sm")
@@ -64,8 +64,8 @@ def lemmatize_text(text):
     lemmatized_text = ' '.join([token.lemma_ for token in doc])
     return lemmatized_text
 
-# DataFrame의 요약(원문) 컬럼에 적용
-df['요약(원문)'] = df['요약(원문)'].astype(str).apply(lemmatize_text)
+# DataFrame의 요약 컬럼에 적용
+df['요약'] = df['요약'].astype(str).apply(lemmatize_text)
 
 # 명사의 원형 추출 함수 정의
 def extract_noun_words(text):
@@ -73,8 +73,8 @@ def extract_noun_words(text):
     noun_words = [token.text for token in doc if token.pos_ == "NOUN"]
     return ' '.join(noun_words).strip()
 
-# DataFrame의 '요약(원문)' 열에 적용하여 명사만 추출
-df['요약(원문)'] = df['요약(원문)'].astype(str).apply(extract_noun_words)
+# DataFrame의 '요약' 열에 적용하여 명사만 추출
+df['요약'] = df['요약'].astype(str).apply(extract_noun_words)
 
 
 
@@ -85,7 +85,7 @@ replacements = {
 }
 # 단어 대체 적용
 for key, value in replacements.items():
-    df['요약(원문)'] = df['요약(원문)'].str.replace(key, value, case=False)
+    df['요약'] = df['요약'].str.replace(key, value, case=False)
 
 # '출원일' 열의 형식 변환
 df['출원일'] = pd.to_datetime(df['출원일'], errors='coerce')
@@ -101,14 +101,14 @@ def create_bigrams(text):
     bigram_list = ['_'.join(bigram) for bigram in bigrams(word_tokens)]
     return ' '.join(bigram_list)
 
-# '요약(원문)' 열에 적용하여 bigram 생성
-df['요약(원문)_bigrams'] = df['요약(원문)'].astype(str).apply(create_bigrams)
+# '요약( 열에 적용하여 bigram 생성
+df['요약_bigrams'] = df['요약'].astype(str).apply(create_bigrams)
 
 # 연도별, 기술별로 그룹화하고 키워드 빈도분석
-keyword_freq = df.groupby(['기술', df['출원일'].dt.year])['요약(원문)'].apply(lambda x: ' '.join(x)).apply(lambda x: pd.Series(nltk.FreqDist(word_tokenize(x.upper())))).unstack().fillna(0)
+keyword_freq = df.groupby(['기술', df['출원일'].dt.year])['요약'].apply(lambda x: ' '.join(x)).apply(lambda x: pd.Series(nltk.FreqDist(word_tokenize(x.upper())))).unstack().fillna(0)
 
 # bigram에 대한 빈도분석
-keyword_freq_bigrams = df.groupby(['기술', df['출원일'].dt.year])['요약(원문)_bigrams'].apply(lambda x: ' '.join(x)).apply(lambda x: pd.Series(nltk.FreqDist(word_tokenize(x.upper())))).unstack().fillna(0)
+keyword_freq_bigrams = df.groupby(['기술', df['출원일'].dt.year])['요약_bigrams'].apply(lambda x: ' '.join(x)).apply(lambda x: pd.Series(nltk.FreqDist(word_tokenize(x.upper())))).unstack().fillna(0)
 
 # 전치하여 행과 열을 바꾸고 CSV 파일로 저장
 keyword_freq_transposed = keyword_freq.T
@@ -118,4 +118,4 @@ keyword_freq_bigrams_transposed = keyword_freq_bigrams.T
 merged_df = pd.concat([keyword_freq_transposed, keyword_freq_bigrams_transposed], axis=1)
 
 # 합친 DataFrame을 CSV 파일로 저장
-merged_df.to_csv("C:/Users/dnjsr/Desktop/특허 데이터 분석 참고 자료/코드/biagram_키워드_빈도분석.csv")
+merged_df.to_csv("C:\\Users\\dnjsr\\Desktop\\캠퍼스 유니버시아드\\2024년_캠퍼스_특허유니버시아드_특허데이터_한국특허전략개발원.csv")
